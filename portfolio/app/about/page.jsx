cc3bddda599556e7fbcf1263d2051bd8e7658594
Ayush Sonone <90081700+ayushsonone07@users.noreply.github.com>
2023-06-10 05:16:30 +0000

    Update 5 files

--- portfolio/app/about/page.jsx
+++ portfolio/app/about/page.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import Link from "next/link";
 
 const aboutPage = () => {
   return (
-    <div>
-      <h1>ABOUT PAGE</h1>
+    <div className="lg:mx-16 mx-20">
+      <h1 className="lg:text-7xl from-zinc-700 font-bold text-center text-3xl m-0 flex-start ">
+        <span className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent">
+          About me
+        </span>
+      </h1>
+      <br />
+      <h3>
+        My name is Ayush. i am a Passionate web developer. who eager to learn
+        new things. i have just completed my graduation from Global Engneering
+        collage jabalpur fom Electronics and communication Engneering
+      </h3>
+      <br />
+      <h3>
+        Strong in design and integration with intuitive problem-solving skills.
+        Proficient in C, PYTHON, JAVASCRIPT, SQL and NoSQL.
+        <br /> I have also larnn some modern technologies like REACT JS, NEXT
+        js, GIT, LINUX. Passionate about implementing and launching new
+        projects. Ability to translate business requirements into technical
+        solutions.
+      </h3>
+      <br />
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full content-center items-center justify-center ">
+        <Link href="https://www.canva.com/design/DAFbkEZH0XE/SWRLxRalyUprebNtzN-q6g/edit?utm_content=DAFbkEZH0XE&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton">
+          Resume
+        </Link>
+      </button>
     </div>
   );
 };
